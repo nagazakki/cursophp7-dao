b18220169f3c7724531d03d31e3dd2f56f24dbac
Alexandre Silva <alexandresoussilva@gmail.com>
2019-05-01 11:51:42 -0300

    Iniciamos o projeto

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+
+spl_autoload_register(function($class_name){
+	
+	spl_autoload(strtolower(str_replace("\\", DIRECTORY_SEPARATOR, $class_name)));
+	
+	$filename = "class".DIRECTORY_SEPARATOR.strtolower($class_name).".php";
+
+	if(file_exists($filename)){
+		require_once($filename);
+	}
+
+});
+
+ ?>
