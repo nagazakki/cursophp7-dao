03d32f9766fb9969d024c615b8fe28eb590e658a
Alexandre Silva <alexandresoussilva@gmail.com>
2019-05-05 20:26:34 -0300

    métodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -39,6 +39,11 @@ class Usuario{
 		$this->dtcadastro = $dtcadastro;
 	}
 
+	public static function getList(){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+	}
+
 	public function loadById($id){
 		$sql = new Sql();
 		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
@@ -55,6 +60,33 @@ class Usuario{
 		}
 
 		
+	}
+
+	public static function search($login){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH'=>"%".$login."%"
+		));
+	}
+
+	public function login($login, $password){
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if(count($results) > 0){
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		}else{
+			throw new Exception("Usuário e/ou senha inválidos");
+		}
 	}
 
 	public function __toString(){
--- index.php
+++ index.php
@@ -2,10 +2,30 @@
 
 	require_once("config.php");
 
-	$root = new Usuario();
+	//Retorna um unico usuário
 
-	$root->loadById(2);
+	// $root = new Usuario();
 
-	echo $root;
+	// $root->loadById(2);
+
+	// echo $root;
+
+	//Retorna uma lista de usuários
+
+	//$lista = Usuario::getList();
+
+	//echo json_encode($lista);
+
+	//Carrega uma lista de usuarios através de um campo de busca
+
+	//$search = Usuario::search("al");
+	//echo json_encode($search);
+
+	//Carrega um usuário autenticado
+
+	$user = new Usuario();
+	$user->login("Paloma", "654321");
+	echo $user;
+
 
  ?>
